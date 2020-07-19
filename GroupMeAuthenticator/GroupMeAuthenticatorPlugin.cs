a6606b8cb4d12582367c9c44ef6617fd4e7ea530
Alex Dillon <6540981+alexdillon@users.noreply.github.com>
2020-07-19 01:19:03 -0500

    Added basic template for a WPF plugin with support for numbering releases based on git tags

---
+++ GroupMeAuthenticator/GroupMeAuthenticatorPlugin.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using GroupMeClientApi.Models;
+using GroupMeClientPlugin;
+using GroupMeClientPlugin.GroupChat;
+
+namespace GroupMeAuthenticator
+{
+    public class GroupMeAuthenticatorPlugin : PluginBase, IGroupChatPlugin
+    {
+        public string PluginName => this.PluginDisplayName;
+
+        public override string PluginDisplayName => "GroupMe Authenticator";
+
+        public override string PluginVersion => Assembly.GetExecutingAssembly().GetName().Version.ToString();
+
+        public override Version ApiVersion => new Version(2, 0, 0);
+
+        public Task Activated(IMessageContainer groupOrChat, CacheSession cacheSession, IPluginUIIntegration integration, Action<CacheSession> cleanup)
+        {
+            var mainWindow = new MainWindow(); // application entry point
+            var vm = new MainWindowViewModel(groupOrChat, cacheSession); 
+            mainWindow.DataContext = vm;
+
+            System.Windows.Application.Current.Dispatcher.Invoke(() =>
+            {
+                mainWindow.ShowDialog();    
+            });
+
+            mainWindow.Closing += (s, e) =>
+            {
+                cleanup(cacheSession);
+            };
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ GroupMeAuthenticator/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,34 @@
+﻿using GroupMeClientApi.Models;
+using GroupMeClientPlugin;
+using System.Linq;
+
+namespace GroupMeAuthenticator
+{
+    class MainWindowViewModel : GalaSoft.MvvmLight.ViewModelBase
+    {
+        private string someGeneratedString;
+
+        public MainWindowViewModel(IMessageContainer messageContainer, CacheSession cacheSession)
+        {
+            this.MessageContainer = messageContainer;
+            this.CacheSession = cacheSession;
+
+            var mostRecentGroupMessage = this.CacheSession.GlobalCache.OrderByDescending(m => m.CreatedAtUnixTime).First();
+            var mostRecentGlobalMessage = this.CacheSession.CacheForGroupOrChat.OrderByDescending(m => m.CreatedAtUnixTime).First();
+
+            this.SomeGeneratedString = $"The most recent message in this group was {mostRecentGroupMessage.Text}. The most recent message in the cache from all groups is {mostRecentGlobalMessage.Text}";
+        }
+
+        public string Name => this.MessageContainer.Name;
+
+        public string SomeGeneratedString
+        {
+            get => this.someGeneratedString;
+            private set => this.Set(() => this.SomeGeneratedString, ref this.someGeneratedString, value);
+        }
+
+        private IMessageContainer MessageContainer { get; }
+
+        private CacheSession CacheSession { get; }
+    }
+}
